ec9de0889137392c0a0e82b0f48033f2f8dd37f5
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-01 23:49:32 +0545

    chore(student.module): imports

--- backend/src/student/student.module.ts
+++ backend/src/student/student.module.ts
@@ -3,9 +3,10 @@ import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentEntity } from './entities/student.entity';
+import { SettingsModule } from 'src/settings/settings.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([StudentEntity])],
+  imports: [TypeOrmModule.forFeature([StudentEntity]), SettingsModule],
   controllers: [StudentController],
   providers: [StudentService],
 })

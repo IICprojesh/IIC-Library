7c32f2fae6bb2717d7e6295424da8c4a5737425c
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-02 20:51:06 +0545

    chore(main): add globbal validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      skipMissingProperties: true,
+    }),
+  );
   const PORT = 3500;
   await app.listen(PORT, () => {
     console.log(`Backend server is running on port ${PORT}`);

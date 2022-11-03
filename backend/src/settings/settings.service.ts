f39a30b7c2a797e3d7cac66a3af4b7de3795f783
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-03 16:49:29 +0545

    refactor(settings): change service functionality

--- backend/src/settings/settings.service.ts
+++ backend/src/settings/settings.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateSettingDto } from './dto/create-setting.dto';
 import { UpdateSettingDto } from './dto/update-setting.dto';
 import { SettingEntity } from './entities/setting.entity';
+import { Blob } from 'node:buffer';
 
 @Injectable()
 export class SettingsService {
@@ -22,11 +23,33 @@ export class SettingsService {
 
   async findOne() {
     const setting = await this.settingRepository.find();
-    if (setting.length) return setting[0];
+    if (setting.length)
+      return {
+        ...setting[0],
+      };
     return null;
   }
 
   update(id: number, updateSettingDto: UpdateSettingDto) {
     return this.settingRepository.update({ id }, updateSettingDto);
   }
+
+  async uploadProfile(profile: Express.Multer.File) {
+    const settings = await this.findOne();
+    if (!settings)
+      throw new BadRequestException('Setting instance is not defined.');
+
+		settings.adminProfile = profile.buffer.toString('base64');
+		return this.settingRepository.save(settings);
+  }
+
+	async getProfile() {
+		const settings = await this.findOne();
+    if (!settings)
+      throw new BadRequestException('Setting instance is not defined.');
+		return {
+			...settings,
+			adminProfile: `data:image/jpeg;base64, ${settings.adminProfile}`,
+		};
+	}
 }

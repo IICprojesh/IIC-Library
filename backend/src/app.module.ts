c0b898b7ea960c0b5e7ddc707d66ece68e87d7bf
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-03 23:03:43 +0545

    chore: remove non-needed module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,20 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
-import { BookModule } from './book/book.module';
-import { AuthorModule } from './author/author.module';
-import { BorrowModule } from './borrow/borrow.module';
-import { RenewModule } from './renew/renew.module';
 import { SettingsModule } from './settings/settings.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     StudentModule,
-    BookModule,
-    AuthorModule,
-    BorrowModule,
-    RenewModule,
     SettingsModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',

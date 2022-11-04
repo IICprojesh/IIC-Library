1e90e149d5065fa7340d38495371ddf21121158b
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-04 11:48:30 +0545

    refactor!

--- frontend/src/Components/Nav/mini-component/Navitems.tsx
+++ frontend/src/Components/Nav/mini-component/Navitems.tsx
@@ -1,31 +1,25 @@
-import * as React from 'react';
-import { Link } from "react-router-dom";
-import styles from './Navitems.module.css';
-import { RiDashboardLine } from 'react-icons/ri';
-import { TbBooks } from 'react-icons/tb';
-import { AiOutlineUser,AiOutlineSetting } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+import { CustomIconType, Icon } from "../../common/icons/Icon";
+import styles from "./Navitems.module.css";
 
-const icons = {
-  dashboard: RiDashboardLine,
-  books: TbBooks,
-  student: AiOutlineUser,
-  setting: AiOutlineSetting,
-
-
-}
-
-
-
-export default function Navitems(props: { icon: keyof typeof icons, text: string, link:string }) {
-  const Icon = icons[props.icon]
+export default function Navitems(props: {
+  icon: CustomIconType;
+  text: string;
+  link: string;
+}) {
   return (
-    <div >
-      <Link to={props.link} className={styles.Navitems}>
-        <Icon className={styles.icon}/>
-        <span className='text'>{props.text}</span>
-      </Link>
-
+    <div>
+      <NavLink
+        to={props.link}
+        className={({ isActive }) => {
+          let str = `${styles.Navitems}`;
+          if (isActive) str += ` ${styles.NavItemsActive}`;
+          return str;
+        }}
+      >
+        <Icon icon={props.icon} className={styles.icon} />
+        <span className="text">{props.text}</span>
+      </NavLink>
     </div>
-
-  )
+  );
 }

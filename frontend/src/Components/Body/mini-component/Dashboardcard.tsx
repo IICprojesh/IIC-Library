3c1312b26d8b7d6f20e52e89346c71fc2e2552dc
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-04 12:04:26 +0545

    refactor(Dashboard)

--- frontend/src/Components/Body/mini-component/Dashboardcard.tsx
+++ frontend/src/Components/Body/mini-component/Dashboardcard.tsx
@@ -1,24 +1,16 @@
+import { CustomIconType, Icon } from "../../common/icons/Icon";
 import styles from "./Dashboardcard.module.css";
-import { AiOutlineFileAdd, AiOutlineBook } from "react-icons/ai";
-import { HiOutlineUsers } from "react-icons/hi";
-
-const icons = {
-  books: AiOutlineBook,
-  add: AiOutlineFileAdd,
-  students: HiOutlineUsers,
-};
 
 export default function Card(props: {
   title: string;
   subtitle: string;
-  icon: keyof typeof icons;
+  icon: CustomIconType;
 }) {
-  const Icon = icons[props.icon];
   return (
     <div className={styles.Container}>
       <div className={styles.carditems}>
         <p className={styles.title}>{props.title}</p>
-        <Icon className={styles.icon} />
+        <Icon icon={props.icon} className={styles.icon} />
       </div>
       <p className={styles.subtitle}>{props.subtitle}</p>
     </div>

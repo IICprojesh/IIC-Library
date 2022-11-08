bc9baa2e16183c4cfc72341ee467c1ca162f22ba
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-08 22:48:44 +0545

    chore(dto): modify issues dto

--- backend/src/issues/dto/create-issue.dto.ts
+++ backend/src/issues/dto/create-issue.dto.ts
@@ -1,4 +1,9 @@
+import { IsISBN, IsString } from 'class-validator';
+
 export class CreateIssueDto {
+  @IsString()
   studentId: string;
+
+  @IsISBN()
   bookId: string;
 }
--- backend/src/issues/dto/update-issue.dto.ts
+++ backend/src/issues/dto/update-issue.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateIssueDto } from './create-issue.dto';
 
-export class UpdateIssueDto extends PartialType(CreateIssueDto) {}
+export class UpdateIssueDto extends PartialType(CreateIssueDto) {
+  @IsOptional()
+  @IsBoolean()
+  returned: boolean;
+}

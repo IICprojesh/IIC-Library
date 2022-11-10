0b5f3b00c30f3ec3e0465c5853721affe32ac30c
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-10 23:23:24 +0545

    chore(utils): add fetchData network fetch utility

---
+++ frontend/src/utils/fetch.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosRequestConfig } from "axios";
+
+const URI: string = process.env.REACT_APP_BACKEND_API!;
+
+export function fetchData(
+  uri: string,
+  options?: AxiosRequestConfig<any> | undefined
+) {
+  return axios(`${URI}/${uri}`, options).then((data) => data.data);
+}

ed93aa51fc53b692c13b70edd9e4f41307ca2584
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-10 12:25:25 +0545

    chore: use PartialType from @nestjs/swagger

--- backend/src/issues/dto/update-issue.dto.ts
+++ backend/src/issues/dto/update-issue.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateIssueDto } from './create-issue.dto';
 
--- backend/src/renew/dto/update-renew.dto.ts
+++ backend/src/renew/dto/update-renew.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateRenewDto } from './create-renew.dto';
 
 export class UpdateRenewDto extends PartialType(CreateRenewDto) {}

73957edebc2ac3e981358bde2a464518bc1c0d89
sewakgtm <sewak.gautam58@gmail.com>
2022-11-17 00:14:56 +0545

    🐞 fix: Delete book
    User can now delete book #7

--- frontend/src/Components/Body/pages/Books/BooksTable.tsx
+++ frontend/src/Components/Body/pages/Books/BooksTable.tsx
@@ -1,16 +1,35 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useEffect, useState, forwardRef } from "react";
 
 import styles from "./BooksTable.module.css";
 import { Button, TextField } from "@mui/material";
 import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { NavLink, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+
+const Transition = forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function BooksTable() {
   const navigate = useNavigate();
 
   const [books, setBooks] = useState<any>(null);
+  const [canDeleteIsbn, setCanDeleteIsbn] = useState("");
+  const [deleteDialogue, setDeleteDialogue] = useState<any>(false);
 
   useEffect(() => {
     axios({
@@ -26,15 +45,47 @@ export default function BooksTable() {
   }, []);
   console.log();
 
-  const handleEdit = (each: any) => {
-    navigate(`/Student/${each.isbn}`);
+  const handleEdit = (each: any) => {};
+
+  const handleDelete = (each: any) => {
+    setCanDeleteIsbn(each.isbn);
+    setDeleteDialogue(true);
   };
-  const handleDelete = () => {
-    console.log("hello everyone");
+  const handleDeleteYes = () => {
+    axios({
+      method: "delete",
+      url: `http://localhost:3500/books/${canDeleteIsbn}`,
+    })
+      .then((res) => {
+        toast.success("Book deleted successfully");
+        setCanDeleteIsbn("")
+        setDeleteDialogue(false)
+      })
+      .catch((err) => {
+        console.log(err)
+        toast.error(err.response.data.message);
+        setDeleteDialogue(false)
+
+      });
+  };
+  const handleDeleteNo = () => {
+    setDeleteDialogue(false);
   };
 
   return (
     <>
+      <Dialog
+        open={deleteDialogue}
+        TransitionComponent={Transition}
+        keepMounted
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>Are You Sure You Want to Delete this Book ?</DialogTitle>
+        <DialogActions>
+          <Button onClick={handleDeleteYes}>Yes</Button>
+          <Button onClick={handleDeleteNo}>NO</Button>
+        </DialogActions>
+      </Dialog>
       <table className={styles.container}>
         <thead>
           <tr>
@@ -51,7 +102,13 @@ export default function BooksTable() {
               <>
                 <tr key={each.id} className={styles.tablerow}>
                   <td key={each.id} className={styles.tabledata}>
-                    <img height="50px" src={each.image} />
+                    <img
+                      height="50px"
+                      width="50px"
+                      style={{ objectFit: "cover" }}
+                      src={each.image}
+                      alt={each.title}
+                    />
                   </td>
                   <td className={styles.tabledata}>{each.isbn}</td>
                   <td className={styles.tabledata}>{each.title}</td>
@@ -60,7 +117,7 @@ export default function BooksTable() {
                     <Button variant="text" onClick={() => handleEdit(each)}>
                       <DriveFileRenameOutlineIcon color="primary" />
                     </Button>
-                    <Button variant="text" onClick={handleDelete}>
+                    <Button variant="text" onClick={() => handleDelete(each)}>
                       <DeleteOutlineIcon color="error" />
                     </Button>
                   </td>

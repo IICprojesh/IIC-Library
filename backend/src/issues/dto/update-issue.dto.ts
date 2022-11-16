86927caf8f992d2bf200d1768688c2a6c3948361
xSjnxyz <mailme.suzan77@gmail.com>
2022-11-16 21:21:55 +0545

    refactor: only allow return or renew updates

--- backend/src/issues/dto/update-issue.dto.ts
+++ backend/src/issues/dto/update-issue.dto.ts
@@ -1,9 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
 import { IsBoolean, IsOptional } from 'class-validator';
-import { CreateIssueDto } from './create-issue.dto';
 
-export class UpdateIssueDto extends PartialType(CreateIssueDto) {
+export class UpdateIssueDto {
   @IsOptional()
   @IsBoolean()
-  returned: boolean;
+  returned?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  renew?: boolean;
 }
